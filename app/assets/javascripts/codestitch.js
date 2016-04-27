44991e8e65a082a41f77fba6c07d982b09de1699
Peter Villeroy <peter.villeroy@gmail.com>
2016-04-27 10:09:17 -0500

    javascript tmp file cleanup

--- app/assets/javascripts/codestitch.js
+++ app/assets/javascripts/codestitch.js
@@ -24,13 +24,3 @@ function setCurrentLevel(cs_lvl) {
     csCurrentLevel = cs_lvl;
     setCookie("csLevel", cs_lvl, 7300); //20 years
 }
-
-//Google Analytics
-(function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
-  (i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
-  m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
-  })(window,document,'script','https://www.google-analytics.com/analytics.js','ga');
-
-  ga('create', 'UA-76776475-1', 'auto');
-  ga('send', 'pageview');
-
