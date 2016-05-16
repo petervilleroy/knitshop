f362136a0cd200eca4595e45f43e2841246a7ed1
Peter Villeroy <peter.villeroy@gmail.com>
2016-05-16 09:24:44 -0400

    User login and logout is functional. On to authorizations...

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -3,9 +3,20 @@ class SessionsController < ApplicationController
   end
 
   def create
-    user = User.find_by_email(params[:email])
+    user = User.find_by_email(params[:email].downcase)
     if user && user.authenticate(params[:password])
+      # login user
       session[:user_id] = user.id
+      
+      #if params[:session][:remember_me] == '1' 
+	# remember user
+        user.remember
+        cookies.permanent.signed[:user_id] = user.id
+        cookies.permanent[:remember_token] = user.remember_token
+      #else
+#	user.forget
+      #end
+      
       redirect_to '/cs'
     else
       render :new
@@ -13,6 +24,9 @@ class SessionsController < ApplicationController
   end
 
   def destroy
+    current_user.forget 
+    cookies.delete(:user_id)
+    cookies.delete(:remember_token)
     session[:user_id] = nil
     redirect_to root_url, notice: 'Logged out!'
   end
