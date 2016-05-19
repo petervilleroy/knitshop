58aba0334e71c9c8a072c23f2cdfb6faa0aa83a6
Peter Villeroy <peter.villeroy@gmail.com>
2016-05-19 15:09:03 -0400

    Solution saved after every successful level completion - ugly but it works

--- app/controllers/solutions_controller.rb
+++ app/controllers/solutions_controller.rb
@@ -1,4 +1,20 @@
 class SolutionsController < ApplicationController
+  def new
+    @solution = Solution.new
+  end
+
+  def create
+    @solution = current_user.solutions.new(level: params[:level], content: params[:content])
+    respond_to do |format|
+      @solution.save
+      format.html { redirect_to root_url }
+      format.json {render :show, status: :created, location: @solution }
+    end
+    
+  end
+
+  def destroy
+  end
 
   # GET /solutions
   def index
@@ -9,4 +25,9 @@ class SolutionsController < ApplicationController
   def show
   end
 
+  private
+  
+  def solution_params
+    params.require(:level, :content).permit(:user_id)
+  end
 end
