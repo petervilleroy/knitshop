7310324a1144835e2402ef5e811c627ec1f27843
Peter Villeroy <peter.villeroy@gmail.com>
2016-06-23 06:45:30 -0400

    Users can now see a table with their saved patterns by clicking on the Patterns Drawer.

--- app/controllers/patterns_controller.rb
+++ app/controllers/patterns_controller.rb
@@ -3,10 +3,10 @@ class PatternsController < ApplicationController
     @pattern = Pattern.new
   end
   def create
-    if params[:solution_id] == 0 do
+    if params[:solution_id] == '0' then 
       @pattern = current_user.patterns.new(solution_id: current_user.solutions.last.id)
-    end
-    else
+    
+    else 
       @pattern = current_user.patterns.new(solution_id: params[:solution_id])    end  
     respond_to do |format|
       @pattern.save
@@ -16,4 +16,15 @@ class PatternsController < ApplicationController
     
   end
 
+  # GET /patterns
+  def index
+    @user = current_user
+    #Return all solutions saved by this user, sorted by level and username?
+    @patterns = Solution.find_by_sql("SELECT * from patterns INNER JOIN solutions ON solutions.id = patterns.solution_id WHERE patterns.user_id = #{@user.id} ").take(125)
+
+    respond_to do |format|
+      format.html { render @patterns }
+      format.json { render json: @patterns }
+    end
+  end
 end
